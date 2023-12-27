b0fe44a6b29268a6b98c40599a8e3179bb39237e
kimura.coki <kimura.coki@gmail.com>
2023-12-27 17:24:28 +0900

    Add CORS mapping URL

--- src/main/java/jp/co/coki/kimura/config/WebMvcConfig.java
+++ src/main/java/jp/co/coki/kimura/config/WebMvcConfig.java
@@ -28,7 +28,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173/")
+                .allowedOrigins("http://localhost:5173/", "http://127.0.0.1:5173/", "http://localhost:8080/",
+                        "http://127.0.0.1:8080/")
                 .allowedMethods("GET", "POST")
                 .allowedHeaders("Authorization");
     }
